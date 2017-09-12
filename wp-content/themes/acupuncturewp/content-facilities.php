107c28379fbe155c767d64ee544e3bfe51b4524d
Jonathan Ceresa <hotstepahh@hotmail.com>
2017-09-12 16:28:43 -0400

    Update to the facilities section to accomodate dynamic content

--- wp-content/themes/acupuncturewp/content-facilities.php
+++ wp-content/themes/acupuncturewp/content-facilities.php
@@ -1,22 +1,42 @@
 <div class="row facilities">
   <div class="col-sm-3">
     <a href="#">
-      <img src="http://via.placeholder.com/100x100" alt="Service 1">Service 1
+      <?php
+        $image = get_field('service_1_image');
+        if( !empty($image) ): ?>
+        	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      <?php endif; ?>
+      <?php the_field('service_1_name') ?>
     </a>
   </div> <!-- /col -->
   <div class="col-sm-3">
     <a href="#">
-      <img src="http://via.placeholder.com/100x100" alt="Service 2">Service 2
+      <?php
+        $image = get_field('service_2_image');
+        if( !empty($image) ): ?>
+        	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      <?php endif; ?>
+      <?php the_field('service_2_name') ?>
     </a>
   </div> <!-- /col -->
   <div class="col-sm-3">
     <a href="#">
-      <img src="http://via.placeholder.com/100x100" alt="Service 3">Service 3
+      <?php
+        $image = get_field('service_3_image');
+        if( !empty($image) ): ?>
+        	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      <?php endif; ?>
+      <?php the_field('service_3_name') ?>
     </a>
   </div> <!-- /col -->
   <div class="col-sm-3">
     <a href="#">
-      <img src="http://via.placeholder.com/100x100" alt="Service 4">Service 4
+      <?php
+        $image = get_field('service_4_image');
+        if( !empty($image) ): ?>
+        	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      <?php endif; ?>
+      <?php the_field('service_4_name') ?>
     </a>
   </div> <!-- /col -->
 </div> <!-- /row -->
